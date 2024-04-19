8a90de3b2bca0a948f4eb9aebab784802bf3c89d
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-04-19 14:47:04 +0000

    [controller作成]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: { minimum: 2 }
 end
+
+# name：一意性を持たせ、かつ2～20文字の範囲で設定してください。
+# introduction：最大50文字までに設定してください。
--- db/migrate/20240419122327_devise_create_users.rb
+++ db/migrate/20240419122327_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :na,              null: false, default: ""
+      t.string :name,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
