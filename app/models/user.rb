db9898e8be83e1a2ed99a4e421bd31518608fa76
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-04-21 14:44:33 +0000

    [ファイル設定]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, length: { minimum: 2 }
+  #validates :name, uniqueness: true, length: {minimum: 2..20}
+  #validates :introduction, length: { maximum: 50 }
+  has_many :books, dependent: :destroy
 end
 
 # name：一意性を持たせ、かつ2～20文字の範囲で設定してください。
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  
+  root to: "homes#top"
   devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  #get 'books/new'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+  #get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
